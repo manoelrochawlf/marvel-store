4be9ed512bc1cbd48120cb5c5e1b2e8b466dc179
Manoel Rocha <manoelrocha399@gmail.com>
2023-03-22 17:39:04 -0300

    feat(Home): home page sketch

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+const Home = () => {
+    return ( 
+        <Container>
+            
+        </Container>
+     );
+}
+ 
+export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background: linear-gradient(120deg, rgba(0,0,0,1) 69%, rgba(99,0,0,1) 95%);
+`
