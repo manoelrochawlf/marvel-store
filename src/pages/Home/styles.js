3b3dbb4a374256f8ec8da0f98ef69698f9554d9c
Manoel Rocha <manoelrocha399@gmail.com>
2023-03-23 10:29:48 -0300

    chore: render tests for api consumption

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,3 +5,18 @@ export const Container = styled.div`
   width: 100%;
   background: linear-gradient(120deg, rgba(0,0,0,1) 69%, rgba(99,0,0,1) 95%);
 `
+export const ComicsCards = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 15rem;
+    height: 13rem;
+    margin: 0.3rem;
+    gap: 1rem;
+    background-color: #71c2bb;
+    color: white;
+    border-radius: 10%;
+    cursor: pointer;
+`
+
