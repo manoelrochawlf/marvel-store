bcfa457630f70537995d541b5a41d8d23a13f77a
Manoel Rocha <manoelrocha399@gmail.com>
2023-03-24 23:02:25 -0300

    chore: comic request limit

--- src/services/ComicsServices.js
+++ src/services/ComicsServices.js
@@ -3,7 +3,7 @@ import Api from "../helpers/Api/axios-config";
 export const ComicsService = {
   getAllComics: async () => {
     try {
-      const res = await Api.get("/comics");
+      const res = await Api.get("/comics?limit=50");
       return res.data;
     } catch (error) {
       console.log(error);
