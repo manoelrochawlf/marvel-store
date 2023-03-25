fd83e1e2dedbfe26e6d853c30151fc623d7d9146
Manoel Rocha <manoelrocha399@gmail.com>
2023-03-24 23:00:32 -0300

    refactor: code refactoring and cleanup

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,33 +1,9 @@
-import { Container, ComicsCards } from "./styles";
-import { useState, useEffect } from "react";
-import { ComicsService } from "../../services/ComicsServices";
-
+import { Container } from "./styles";
+import ComicsCards from "../../components/ComicsCards";
 const Home = () => {
-
-    const [comics, setComics] = useState([]);
-    useEffect(() => {
-        const getComics = async () => {
-          try {
-            const response = await  ComicsService.getAllComics();
-            console.log(response)
-            if (response && response.data) {
-              setComics(response.data.results);
-            }
-          } catch (error) {
-            console.log(error);
-          }
-        };
-        getComics();
-      }, []);
-    
-
     return ( 
         <Container>
-            {comics.map((comics) => (
-            <ComicsCards key={comics.id}>
-                {comics.title}
-            </ComicsCards>
-            ))}
+              <ComicsCards />      
         </Container>
      );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,22 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
   height: 100vh;
   width: 100%;
-  background: linear-gradient(120deg, rgba(0,0,0,1) 69%, rgba(99,0,0,1) 95%);
+  padding: 2rem 4rem;
+  background: black;
 `
-export const ComicsCards = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 15rem;
-    height: 13rem;
-    margin: 0.3rem;
-    gap: 1rem;
-    background-color: #71c2bb;
-    color: white;
-    border-radius: 10%;
-    cursor: pointer;
-`
+
 
