8a9252c9a08254954ade8a9b909b03b353724070
Manoel Rocha <manoelrocha399@gmail.com>
2023-04-05 14:48:58 -0300

    chore: add route of shopcart

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ComicsDetails from './components/ComicsDetails';
+import ShopCart from './components/ShopCart';
 import Home from './pages/Home';
 
 const Router = () => {
@@ -8,6 +9,7 @@ const Router = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/comics/:id" element={<ComicsDetails />} />
+            <Route path="/shoppingcart" element={<ShopCart />} />
         </Routes>
     );
 }
